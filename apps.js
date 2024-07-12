7e8e95c7c451215c23fb22ffc0ee6f846b70dfc3
Lisa Bridges <lbridges@staff.wvnet.edu>
2024-07-12 12:03:27 -0400

    testing popover

--- apps.js
+++ apps.js
@@ -22,7 +22,12 @@
             document.addEventListener('DOMContentLoaded', function() {
               var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
               var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-                return new bootstrap.Popover(popoverTriggerEl)
+                return new bootstrap.Popover(popoverTriggerEl, {
+                  html: true,
+                  content: function() {
+                    return document.getElementById('popover-content').innerHTML;
+                  }
+                })
               })
             });
-          </script>
+            </script>
