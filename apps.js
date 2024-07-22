19766f473a174a66636f27d288bf37ca3cb7d4a1
Lisa Bridges <lbridges@staff.wvnet.edu>
2024-07-22 08:30:12 -0400

    reverting

--- apps.js
+++ apps.js
@@ -1,5 +1,18 @@
 
+// global variables
+gridContainer.addEventListener('click', e => {
+    // make sure the click is not on the gridContainer itself
+    if (e.target !== gridContainer) {
+    // select the card element based on its proximity to actual element clicked
+    const card = e.target.closest(".card");
+    const index = card.getAttribute('data-index');
+    displayModal(index);
+    }
+    });
 
+    modalClose.addEventListener('click', () => {
+        overlay.classList.add("hidden");
+        });
 
 $(function () {
   $('[data-toggle="popover"]').popover()
