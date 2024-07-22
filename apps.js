e663533daa5f43b3bf89201f0ea32a5423c0ec7e
Lisa Bridges <lbridges@staff.wvnet.edu>
2024-07-22 14:14:35 -0400

    removing popover js

--- apps.js
+++ apps.js
@@ -14,9 +14,7 @@ gridContainer.addEventListener('click', e => {
         overlay.classList.add("hidden");
         });
 
-$(function () {
-  $('[data-toggle="popover"]').popover()
-})
+
 
 //Include Bootstrap JS 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
