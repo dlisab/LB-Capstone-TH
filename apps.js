19333fb8792846abda4973f7248eb3e9b104a347
Lisa Bridges <lbridges@staff.wvnet.edu>
2024-07-23 16:10:37 -0400

    adding javascript button

--- apps.js
+++ apps.js
@@ -1,11 +1,4 @@
-document.addEventListener('mousemove', function(event) {
-    const x = event.clientX / window.innerWidth;
-    const y = event.clientY / window.innerHeight;
-    
-    const r = Math.round(x * 255);
-    const g = Math.round(y * 255);
-    const b = Math.round((x + y) / 2 * 255);
-    
-    const color = `rgb(${37}, ${150}, ${190})`;
-    document.getElementById('jumbotron').style.backgroundColor = color;
-});
+document.getElementById("emailButton").addEventListener("click", function () {
+    window.location.href =
+      "mailto:lisabridgesdesign@gmail.com";
+  });
